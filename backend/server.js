f45e33af260198421be065d7ddb3656a5be2c501
arman8464 <armankhan400k@gmail.com>
2026-04-08 20:14:09 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // DB CONNECTION
